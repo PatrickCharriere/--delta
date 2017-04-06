88c8b376bd6de9fab59dd39d404ecc56abfefb51
Patrick Charrière <patrick.charriere.pro@gmail.com>
2017-04-06 08:55:17 +0200

    WIP Weekly imputation calendar

--- app/scripts/controllers/impute.js
+++ app/scripts/controllers/impute.js
@@ -10,8 +10,36 @@
 angular.module('imputationApp')
   .controller('ImputeCtrl', ['$scope', function($scope) {
 	
+  	$scope.companyList=[
+  	{
+  		name:"Ingenico",
+  		BU: {
+  			name:"Professional Services"
+  		}
+  	},{
+  		name:"Astek",
+  		BU: {
+  			name:""
+  		}
+  	}
+  	]
+
+  	$scope.createYearSelector=function(){
+  		var date = new Date();
+  		var year = date.getFullYear();
+  		$scope.years = [];
+  		for(var i = year-10; i <= year; i++) {
+  			$scope.years.push(i);
+  		}
+  		console.log($scope.years)
+  	}
+
 	$scope.init = function(){
+		
 		$scope.weekDays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+		
+		$scope.createYearSelector();
+
 	}
 	$scope.init();
 
